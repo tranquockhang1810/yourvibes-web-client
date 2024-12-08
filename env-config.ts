d16a18e259b549bc870f0b1726ca708e46858de6
Phạm Minh Thảo <108984672+Trunks-Pham@users.noreply.github.com>
2024-12-08 20:19:07 +0700

    TỆ

--- env-config.ts
+++ env-config.ts
@@ -1,14 +1,1 @@
-const ENV = { 
-  // SERVER_ENDPOINT: 'http://192.168.20.101:8080', //student
-  SERVER_ENDPOINT: 'http://192.168.43.180:8080',//4G;
-  // SERVER_ENDPOINT: 'http://192.168.110.81:8080',//Phòng
-  // SERVER_ENDPOINT: 'http://192.168.88.132:8080',//VNPT
-  // SERVER_ENDPOINT: 'http://10.5.50.54:8080',//Passio
-  // SERVER_ENDPOINT: 'http://172.23.139.179:8080',// Highlands
-
-  WEB_CLIENT_ID: "1043707259288-skk9gvug9tlvahvfduf1km8p7dar0osf.apps.googleusercontent.com",
-  ANDROID_CLIENT_ID: "1043707259288-u2rc90r329lmsfrceogq8ti1ouvtm5e2.apps.googleusercontent.com",
-  IOS_CLIENT_ID: "1043707259288-8ae2okgbb1mlmg57br60hlh9g0hgmrfr.apps.googleusercontent.com",
-};
-
-export default ENV;
+// LỠ TAY ĐẨY LÊN ĐÂY HUHU
