20f3ffece23029b91c5918698b70d5e4777ebbe7
MTPE <mtpe2301@gmail.com>
2024-12-26 11:53:39 +0700

    pull master

--- src/api/ApiPath.ts
+++ src/api/ApiPath.ts
@@ -55,19 +55,7 @@ export const ApiPath = {
 function getApiPath(path: string) {
   return `${process.env.NEXT_PUBLIC_API_ENDPOINT!}/v1/2024/${path}`;
 }
-// function getApiPath(path: string) {
-//   return `${ENV.SERVER_ENDPOINT}/v1/2024/${path}`;
-// }
-//Để đở đừng xóa nha =)))
+
 function getWSPath(path: string) {
   return `${process.env.NEXT_PUBLIC_API_ENDPOINT!.replace("http", "ws")!}/v1/2024/${path}`;
 }
-
-//bên tui để vầy mới coi được
-// function getWSPath(path: string) {
-//   return `${process.env.NEXT_PUBLIC_API_ENDPOINT?.replace("http", "ws")}/v1/2024/${path}`;
-// }
-
-// function getWSPath(path: string) {
-//   return `${ENV.SERVER_ENDPOINT.replace("http", "ws")!}/v1/2024/${path}`;
-// }
