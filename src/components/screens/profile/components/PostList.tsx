cc8906630f4ad125872fe8fa84bd4cfab3294455
Tran Quoc Khang <tranquockhang1810@gmail.com>
2025-01-11 15:13:07 +0700

    Build fix

--- src/components/screens/profile/components/PostList.tsx
+++ src/components/screens/profile/components/PostList.tsx
@@ -9,14 +9,13 @@ import { useAuth } from '@/context/auth/useAuth';
 import Post from '@/components/common/post/views/Post';
 import AddPostScreen from '@/components/screens/addPost/view/AddPostScreen';
 
-const PostList = ({ loading, posts, loadMorePosts, user, fetchUserPosts, hasMore, deletePost }: {
+const PostList = ({ loading, posts, loadMorePosts, user, fetchUserPosts, hasMore }: {
   loading: boolean;
   posts: PostResponseModel[];
   loadMorePosts: () => void;
   user: UserModel;
   fetchUserPosts: () => void;
   hasMore: boolean; // Biến để kiểm tra có còn dữ liệu hay không
-  deletePost: (postId: string) => void;
 }) => {
   const { backgroundColor, lightGray } = useColor();
   const { isLoginUser, localStrings } = useAuth();
@@ -26,18 +25,18 @@ const PostList = ({ loading, posts, loadMorePosts, user, fetchUserPosts, hasMore
   useEffect(() => {
     setPostList(posts);
   }, [posts]);
-  
-    // Hàm xóa bài viết khỏi danh sách hiển thị
-    const handleDeletePost = (postId: string) => {
-      setPostList((prev) => prev.filter((post) => post.id !== postId));
-      message.success('Bài viết đã được xóa!');
-    };
+
+  // Hàm xóa bài viết khỏi danh sách hiển thị
+  const handleDeletePost = (postId: string) => {
+    setPostList((prev) => prev.filter((post) => post.id !== postId));
+    message.success('Bài viết đã được xóa!');
+  };
 
 
   const handleModalClose = () => {
-    setIsModalVisible(false); 
+    setIsModalVisible(false);
   };
-  
+
 
   const handlePostSuccess = () => {
     setIsModalVisible(false);
@@ -52,13 +51,13 @@ const PostList = ({ loading, posts, loadMorePosts, user, fetchUserPosts, hasMore
           style={{
             padding: "10px",
             display: "flex",
-            alignItems: "center", 
+            alignItems: "center",
             backgroundColor: backgroundColor,
             border: `1px solid ${lightGray}`,
             borderRadius: "10px",
             cursor: "pointer",
-            width: "100%", 
-            maxWidth: "600px", 
+            width: "100%",
+            maxWidth: "600px",
           }}
         >
           <img
@@ -79,9 +78,9 @@ const PostList = ({ loading, posts, loadMorePosts, user, fetchUserPosts, hasMore
             <p style={{ color: "gray" }}>{localStrings.Public.Today}</p>
           </div>
         </div>
-        <Modal centered title={localStrings.AddPost.NewPost} 
+        <Modal centered title={localStrings.AddPost.NewPost}
           open={isModalVisible} onCancel={() => setIsModalVisible(false)} width={800} footer={null}>
-            <AddPostScreen onPostSuccess={handlePostSuccess} />
+          <AddPostScreen onPostSuccess={handlePostSuccess} />
         </Modal>
       </>
     );
@@ -93,37 +92,37 @@ const PostList = ({ loading, posts, loadMorePosts, user, fetchUserPosts, hasMore
       {isLoginUser(user?.id as string) && renderAddPost()}
 
       {/* Posts List */}
-      <div style={{width: '100%' }}>
-       {posts && posts.length > 0 ? (
-        <InfiniteScroll
-          dataLength={posts.length}
-          next={loadMorePosts}
-          hasMore={hasMore}
-          loader={<Spin indicator={<LoadingOutlined spin />} size="large" />}
-          endMessage={
-            <p style={{ textAlign: 'center' }}>
-              {/* <b>{localStrings.Public.NoMorePosts}</b> */}
-            </p>
-          }
-        >
-         
+      <div style={{ width: '100%' }}>
+        {posts && posts.length > 0 ? (
+          <InfiniteScroll
+            dataLength={posts.length}
+            next={loadMorePosts}
+            hasMore={hasMore}
+            loader={<Spin indicator={<LoadingOutlined spin />} size="large" />}
+            endMessage={
+              <p style={{ textAlign: 'center' }}>
+                {/* <b>{localStrings.Public.NoMorePosts}</b> */}
+              </p>
+            }
+          >
+
             {posts.map((item) => (
               <div key={item?.id} className='w-full flex flex-col items-center' >
                 <Post post={item} fetchUserPosts={() => {
-                    fetchUserPosts(); // Gọi lại API nếu cần
-                    item?.id && handleDeletePost(item.id); // Xóa bài viết khỏi danh sách
-                  }}>
+                  fetchUserPosts(); // Gọi lại API nếu cần
+                  item?.id && handleDeletePost(item.id); // Xóa bài viết khỏi danh sách
+                }}>
                   {item?.parent_post && <Post post={item?.parent_post} isParentPost />}
                 </Post>
               </div>
             ))}
-        </InfiniteScroll>): (
+          </InfiniteScroll>) : (
           <div style={{ textAlign: 'center', padding: '20px' }}>
-              <Empty description={
-                <span style={{ color: 'gray', fontSize: 16 }}>
-                  {localStrings.Post.NoPosts}
-                </span>
-              }
+            <Empty description={
+              <span style={{ color: 'gray', fontSize: 16 }}>
+                {localStrings.Post.NoPosts}
+              </span>
+            }
             />
           </div>
         )}
